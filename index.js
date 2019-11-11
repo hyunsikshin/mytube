80581cb892cf886ef26ca49bbeaa40bf96118828
hyunsik <hyunsik.dev@gmail.com>
2019-11-11 13:55:25 -0500

    Add Middleware

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
 const app = express();
 
@@ -6,10 +10,15 @@ const PORT = 4000;
 
 const handleListening = () => console.log(`listein port ${PORT}`);
 
-const handleHome = (req, res) => {
-  console.log(req);
-  res.send('Hello from index.js toto');
-};
+const handleHome = (req, res) => res.send('Hello from index.js toto');
 
+const handleProfile = (req, res) => res.send('here is profile');
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(helmet());
 app.get('/', handleHome);
+app.get('/profile', handleProfile);
 app.listen(PORT, handleListening);
