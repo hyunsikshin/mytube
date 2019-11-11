d815edb92b9e925ce324199d7d0a21c27c05d818
hyunsik <hyunsik.dev@gmail.com>
2019-11-11 11:54:30 -0500

    Add babel and nodemon

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello')
+import express from 'express';
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => console.log(`listein port ${PORT}`);
+
+const handleHome = (req, res) => {
+  console.log(req);
+  res.send('Hello from index.js toto');
+};
+
+app.get('/', handleHome);
+app.listen(PORT, handleListening);
