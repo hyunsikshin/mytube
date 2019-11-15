351750d1d54920cf782b47acf4c16dc0dff367fe
hyunsik <hyunsik.dev@gmail.com>
2019-11-15 15:54:12 -0500

    Connecting to mongodb

--- db.js
+++ db.js
@@ -1,62 +1,16 @@
-export const videos = [
-  {
-    id: 123412,
-    title: 'The Bear',
-    description: 'The bear in the flow',
-    views: 35,
-    videoFile: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.mp4',
-    creator: {
-      id: 911832,
-      name: 'hyunsik',
-      email: 'hyunsik@is.me',
-    },
-  },
-  {
-    id: 980980,
-    title: 'Avatar',
-    description: 'Blue alian is tall',
-    views: 49,
-    videoFile: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.mp4',
-    creator: {
-      id: 911832,
-      name: 'hyunsik',
-      email: 'hyunsik@is.me',
-    },
-  },
-  {
-    id: 124737,
-    title: 'Inception',
-    description: 'Go to dream in dream',
-    views: 90,
-    videoFile: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.mp4',
-    creator: {
-      id: 911832,
-      name: 'hyunsik',
-      email: 'hyunsik@is.me',
-    },
-  },
-  {
-    id: 947298,
-    title: 'Dark Night',
-    description: 'The batman in godam city',
-    views: 35,
-    videoFile: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.mp4',
-    creator: {
-      id: 911832,
-      name: 'hyunsik',
-      email: 'hyunsik@is.me',
-    },
-  },
-  {
-    id: 264797,
-    title: 'Avengers',
-    description: 'The hero who protect earth',
-    views: 35,
-    videoFile: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.mp4',
-    creator: {
-      id: 911832,
-      name: 'hyunsik',
-      email: 'hyunsik@is.me',
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/mytube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connect to db");
+
+const handleError = error => console.log(`❌ Error on connect DB ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
--- init.js
+++ init.js
@@ -1,4 +1,5 @@
-import app from './app';
+import "./db";
+import app from "./app";
 
 const PORT = 4000;
 
