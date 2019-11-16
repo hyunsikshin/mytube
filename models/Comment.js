589c8386bbe821d7a5093c4ab4880b0b21c3b7f2
hyunsik <hyunsik.dev@gmail.com>
2019-11-16 16:19:31 -0500

    Add Comment model

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: "Text is required"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+  // Comment 랑 Video는 RelationShip 있어서 둘중 하나에 작성해야한다.
+  // 아래는 그 관계를 Comment에 작성한 내용
+  // video: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: "Video"
+  // }
+});
+
+const model = mongoose.model("Comment", CommentSchema);
+
+export default model;
