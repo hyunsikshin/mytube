701d94d957bb7b011b75d62888a4485faa462455
hyunsik <hyunsik.dev@gmail.com>
2019-11-16 21:22:10 -0500

    Add function that edit video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -41,11 +41,48 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
 
-export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: "Video Detail", video });
+  } catch (err) {
+    res.redirect(routes.home);
+  }
+};
+
+// Render
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+  } catch (err) {
+    res.redirect(routes.home);
+  }
+};
+
+//Update
+export const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description }
+  } = req;
+  try {
+    await Video.findOneAndUpdate(
+      { _id: id },
+      { title, description },
+      res.redirect(routes.videoDetail(id))
+    );
+  } catch (err) {
+    res.redirect(routes.home);
+  }
+};
 
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,19 +3,27 @@ import routes from "../routes";
 import { uploadVideo } from "../middleware";
 import {
   videoDetail,
-  editVideo,
   deleteVideo,
   getUpload,
-  postUpload
+  postUpload,
+  getEditVideo,
+  postEditVideo
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+//  UPLOAD
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+//  VIDEO DETAIL
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+//  EDIT VIDEO
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//  DELETE VIDEO
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
