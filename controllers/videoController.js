d66533670b393bbd3bb1e00abbb9477261c0cc7a
hyunsik <hyunsik.dev@gmail.com>
2019-11-16 21:38:41 -0500

    Add function that delete video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -48,7 +48,7 @@ export const videoDetail = async (req, res) => {
 
   try {
     const video = await Video.findById(id);
-    res.render("videoDetail", { pageTitle: "Video Detail", video });
+    res.render("videoDetail", { pageTitle: video.title, video });
   } catch (err) {
     res.redirect(routes.home);
   }
@@ -84,5 +84,15 @@ export const postEditVideo = async (req, res) => {
   }
 };
 
-export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+
+  try {
+    await Video.findOneAndRemove({ _id: id });
+  } catch (err) {
+    console.log(err);
+  }
+  res.redirect(routes.home);
+};
