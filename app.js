85262d8d73dcaf267d5ee21cef24163a01d53877
hyunsik <hyunsik.dev@gmail.com>
2019-11-16 19:53:39 -0500

    Add function that upload and create video

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
-import { localsMiddleWare } from './middleware';
-import morgan from 'morgan';
-import routes from './routes';
-import globalRouter from './routers/globalRouter';
-import userRouter from './routers/userRouter';
-import videoRouter from './routers/videoRouter';
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import { localsMiddleWare } from "./middleware";
+import morgan from "morgan";
+import routes from "./routes";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
 app.use(helmet());
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(localsMiddleWare);
 
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,17 @@
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) =>
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
+};
 
 export const search = (req, res) => {
-  //console.log(req.query);
-  //console.log(req.query.term);
   const {
     query: { term: searchingBy }
   } = req; //같은 내용 const searchingBy = req.query.term;
@@ -18,14 +24,21 @@ export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "Upload" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { ffile, title, description }
+    body: { title, description },
+    file: { path }
   } = req;
 
-  //TODO Upload and save video
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  });
 
-  res.redirect(routes.videoDetail(980980));
+  console.log(newVideo);
+
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
