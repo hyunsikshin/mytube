ced34fdcb1e1f8905b6d87c6f4d8ed393434ceba
hyunsik <hyunsik.dev@gmail.com>
2019-12-08 00:56:36 -0500

    Add creator to Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -44,11 +44,11 @@ export const postUpload = async (req, res) => {
   const newVideo = await Video.create({
     fileUrl: path,
     title,
-    description
+    description,
+    creator: req.user.id
   });
-
-  console.log(newVideo);
-
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -58,7 +58,8 @@ export const videoDetail = async (req, res) => {
   } = req;
 
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator");
+    console.log(video);
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (err) {
     res.redirect(routes.home);
--- models/Comment.js
+++ models/Comment.js
@@ -8,6 +8,10 @@ const CommentSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
   }
   // Comment 랑 Video는 RelationShip 있어서 둘중 하나에 작성해야한다.
   // 아래는 그 관계를 Comment에 작성한 내용
