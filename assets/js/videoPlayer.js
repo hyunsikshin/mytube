24ce75937ae405527fb2ff455b3d73455be91556
hyunsik <hyunsik.dev@gmail.com>
2020-01-07 20:30:13 +0900

    Add FullScreen and Time

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,9 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const playButton = document.getElementById("jsPlayButton");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const volumeButton = document.getElementById("jsVolumeBtn");
+const fullButton = document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -23,13 +26,46 @@ function handleVolumeClick() {
   }
 }
 
+function goFullScreen() {
+  videoPlayer.requestFullscreen();
+}
+
+function formatDate(seconds) {
+  const secondsNumber = parseInt(seconds, 10);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (totalSeconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+
+  return `${hours}:${minutes}:${totalSeconds}`;
+}
+
+function getCurrentTime() {
+  currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+}
+
+function setTotalTime() {
+  const totalTimeString = formatDate(videoPlayer.duration);
+  totalTime.innerHTML = totalTimeString;
+  setInterval(getCurrentTime, 1000);
+}
+
 function init() {
   playButton.addEventListener("click", handlePlayClick);
   volumeButton.addEventListener("click", handleVolumeClick);
+  fullButton.addEventListener("click", goFullScreen);
+  videoPlayer.addEventListener("loadedmetadata", setTotalTime);
 }
 
 if (videoContainer) {
   init();
 }
-
-// videoContainer.addEventListener("click", () => false);
