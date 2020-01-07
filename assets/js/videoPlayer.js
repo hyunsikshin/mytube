3876aef270a64a41df830163366cf07277000e07
hyunsik <hyunsik.dev@gmail.com>
2020-01-07 19:54:42 +0900

    Add Mute Button

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,6 +1,7 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const playButton = document.getElementById("jsPlayButton");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
+const volumeButton = document.getElementById("jsVolumeBtn");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -12,8 +13,19 @@ function handlePlayClick() {
   }
 }
 
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeButton.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeButton.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+}
+
 function init() {
   playButton.addEventListener("click", handlePlayClick);
+  volumeButton.addEventListener("click", handleVolumeClick);
 }
 
 if (videoContainer) {
